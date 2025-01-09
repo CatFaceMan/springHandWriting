fb13f98fe9b30bb08129ca135f16781a1a5fcc64
既明 <2294240601@qq.com>
2025-01-09 16:07:09 +0800

    feat: 实现简单版 Spring bean 容器
    - 新增 BeanDefinition、BeanFactory 类和 UserService 示例类
    - 添加 ApiTest 类演示 bean 容器的使用
    - 创建 IntelliJ IDEA 项目配置文件- 添加 .gitignore 文件忽略不必要的项目文件

---
+++ spring/src/beanContainer/ApiTest.java
@@ -0,0 +1,16 @@
+package beanContainer;
+
+public class ApiTest {
+    public static void main(String[] args) {
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注册 bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取 bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+}
